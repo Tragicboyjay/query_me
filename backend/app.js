9c7f2c92f331a8103ab0d96beb7d1f2beec950b5
Tragicboyjay <124944418+Tragicboyjay@users.noreply.github.com>
2024-05-13 19:43:28 -0400

    set up mongo db connection

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const mongoose = require('mongoose');
 
 // test route
 app.get('/test', (req,res) => {
     res.send('test');
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening at http://localhost:${process.env.PORT}`)
-});
+// Db connection and app start
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Connected to db and listening at http://localhost:${process.env.PORT}/`)
+    });
+})
+.catch(err => console.error(`error: ${err}`))
