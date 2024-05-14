65d8c20197b0891bab8ca0459a41171af62a4071
Tragicboyjay <124944418+Tragicboyjay@users.noreply.github.com>
2024-05-14 19:12:22 -0400

    edited protect middleware + created delete user
    better error handling in middleware

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -1,29 +1,41 @@
 const jwt = require('jsonwebtoken');
 const User = require("../Models/UserModel");
 
-const protect = async function( req, res, next) {
+const protect = async function(req, res, next) {
     let token;
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1];
 
-            const decoded = jwt.verify(token, process.env.process.env.TOKEN_SECRET);
+            const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
 
             req.user = await User.findById(decoded.id).select('-password');
+
+            // Check if req.user exists before accessing its properties
+            if (!req.user) {
+                return res.status(401).json({ message: "Not Authorized" });
+            }
+
+            if (!req.params.id) {
+                return res.status(400).json({ message: 'Bad Request: User ID is required' });
+            }
+
+            if (req.params.id !== req.user._id.toString()) {
+                return res.status(403).json({ message: 'Forbidden: You are not authorized to perform this action' });
+            }
 
             next();
             
         } catch (error) {
-            console.log(error)
-            res.status(401).json({message: "Not Authorized"});
+            console.log(error);
+            res.status(401).json({ message: "Not Authorized" });
         }
     }
 
     if (!token) {
-        res.status(401).json({message: "Not Authorized, no token"});
+        res.status(401).json({ message: "Not Authorized, no token" });
     }
-    
 };
 
-module.exports = { protect }
+module.exports = { protect };
--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../Middleware/authMiddleware');
+
+const {
+    deleteUser
+} = require("../Controllers/UserContoller")
+
+router.delete("/:id", protect, deleteUser);
 
 module.exports = router;
