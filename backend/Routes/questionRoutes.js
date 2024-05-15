78501f5c5b2cb56c4261c3b6dae2ebdcf71c374b
Tragicboyjay <124944418+Tragicboyjay@users.noreply.github.com>
2024-05-15 01:04:43 -0400

    answer question functionality

--- backend/Routes/questionRoutes.js
+++ backend/Routes/questionRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { askQuestion } = require('../Controllers/QuestionController');
+const { askQuestion, answerQuestion } = require('../Controllers/QuestionController');
 const { protect }  = require("../Middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/ask/:username", protect, askQuestion);
+router.post("/answer/:questionId", protect, answerQuestion);
 
 module.exports = router;
--- backend/controllers/QuestionController.js
+++ backend/controllers/QuestionController.js
@@ -37,6 +37,38 @@ const askQuestion = async function (req,res) {
     }    
 };
 
+const answerQuestion = async function (req,res) {
+    try {
+        const questionId = req.params.questionId;
+        const reciever = await User.findById(req.user._id);
+        const {questionAnswer} = req.body
+
+        if (!reciever.questionsRecieved.includes(questionId)){
+            questionLogger.error(`Status code: 403, Message: 'Unauthorized: You are not authorized to answer this question. Question ID: ${questionId}'`);
+            res.status(403).json({ message: 'You are not authorized to answer this question.', questionId });
+        };
+
+        const question = await Question.findById(questionId);
+
+        if (question.answer) {
+            questionLogger.error(`Status code: 400, Message: 'Error answering question: Question has already been answered. Question ID: ${questionId}'`);
+            res.status(400).json({ message: 'Question has already been answered' });
+        };
+
+        await Question.findByIdAndUpdate(questionId, { answer: questionAnswer })
+
+        questionLogger.info(`Status code: 200, Message: 'Question answered successfully. Question ID: ${questionId}'`);
+        res.status(200).json({ message: 'Question answered successfully'});
+        
+    } catch (error) {
+        console.error('Error asking question:', error);
+        questionLogger.error("Status code: 500, Message: 'Error answering question: Internal server error'");
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+
 module.exports = {
-    askQuestion
+    askQuestion,
+    answerQuestion
 };
